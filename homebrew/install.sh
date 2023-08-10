9a396a3a1114fd5d611960529562cfc74e31d60c
Slpixe <slpixe@gmail.com>
2023-08-10 12:24:05 +0000

    brew install maybe

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,6 +2,7 @@
 
 if ! type "brew" >/dev/null 2>&1; then
     echo "Homebrew not found, install it first"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     exit 1
 fi
 
