63b5fba694795fe084fb5a48c2479c613cb03024
Slpixe <slpixe@gmail.com>
2023-08-10 12:29:41 +0000

    brew path

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -3,6 +3,35 @@
 if ! type "brew" >/dev/null 2>&1; then
     echo "Homebrew not found, install it first"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Determine the Homebrew installation path
+    brew_path=$(which brew)
+
+    # Determine the user's default shell
+    user_shell=$(basename "$SHELL")
+
+    # Define shell-specific configuration file names
+    case "$user_shell" in
+        bash)
+            shell_config_file=".bashrc"
+            ;;
+        zsh)
+            shell_config_file=".zshrc"
+            ;;
+        *)
+            echo "Unsupported shell: $user_shell"
+            exit 1
+            ;;
+    esac
+
+    # Add Homebrew to PATH if it's not already present
+    if [[ ":$PATH:" != *":$brew_path:"* ]]; then
+        echo "export PATH=$brew_path:\$PATH" >> "$HOME/$shell_config_file"
+        echo "Homebrew added to PATH in $shell_config_file. Restart your shell to apply the changes."
+    else
+        echo "Homebrew is already in your PATH."
+    fi
+
     exit 1
 fi
 
