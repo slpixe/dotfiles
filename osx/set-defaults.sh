a2bfb7896e1db551494d57234dbf3b041de48ea2
Slpixe <slpixe@gmail.com>
2023-08-11 01:09:14 +0000

    autohide dock

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -17,6 +17,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # old
 
+#autohide the dock
+defaults write com.apple.dock "autohide" -bool "true"
+
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
